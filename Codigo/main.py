2f50c9b41523fc8dbef8efdf5371657f01dcf4c6
gosurataibanez@gmail.com <gosurataibanez@gmail.com>
2020-11-05 11:51:53 -0300

    20201105 1151

--- Codigo/main.py
+++ Codigo/main.py
@@ -2,22 +2,32 @@ import numpy as np
 import sympy as sy
 
 
-# vectorCensoBelgica = [[1990,9967379],[2000,10251250],[2010,10895586]]
+vectorCensoBelgica = [[1990,9967379],[2000,10251250],[2010,10895586]]
 # print(vectorCensoBelgica)
 
-p3 = 9967379
+p1 = 9967379
 p2 = 10251250 
-p1 = 10895586
-t3 = 1990
+p3 = 10895586
+t1 = 1990
 t2 = 2000
-t1 = 2010
+t3 = 2010
 
-def f(k):
-    return (((p3 * sy.exp(-(k * t3))) - (p2 * sy.exp(-(k * t2)))) * (p1 - p2)) + ((p2 - p3) * ((p1 * sy.exp(-(k * t1))) - (p2 * sy.exp(-(k * t2)))))
 
-a = -0.08 
-b = -0.07
-iteraciones = 51
+
+
+#def f(k):
+#    return (((p3 * sy.exp(-(k * t3))) - (p2 * sy.exp(-(k * t2)))) * (p1 - p2)) + ((p2 - p3) * ((p1 * sy.exp(-(k * t1))) - (p2 * sy.exp(-(k * t2)))))
+
+def f(k) :
+	return (p1 * (1- sy.exp(-k*t2))- p2*(1-p1*sy.exp(-k*t1)))
+	
+#a = -0.08 
+#b = -0.07
+
+a = 0;
+b = 1;
+
+iteraciones = 50
 
 kRaiz = 0
 
@@ -30,6 +40,6 @@ for k in range(iteraciones):
     else:
         break
     kRaiz = x
-    print(k, x, f(x))
+    print(k,a,b, x, f(x))
 
 print(kRaiz)
